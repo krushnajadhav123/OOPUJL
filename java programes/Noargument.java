961154a602810b660119d4f1dae42cb789105a7a
krushnajadhav123 <kj2913280@gmail.com>
2025-08-10 23:49:56 +0530

    Update and rename Noargumen.java to Noargument.java

--- java programes/Noargument.java
+++ java programes/Noargument.java
@@ -1,15 +1,16 @@
 
-public class Default{
+public class Noargument{
 int roll_no;
 String name;
-public Default(){
+public  Noargument(){
  roll_no=4;
 name="Krushna";
 }
 public static void main(String[] args){
-Default s=new Default();
+ Noargument s=new  Noargument();
 System.out.println("student information");
 System.out.println("student roll no:"+s.roll_no);
 System.out.println("student name:"+s.name);
 }
 }
+
