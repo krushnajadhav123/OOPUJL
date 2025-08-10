593050a0c638625aae09c979be449b5b6bcd0c92
krushnajadhav123 <kj2913280@gmail.com>
2025-08-10 23:45:13 +0530

    Update Parameterize.java

--- java programes/Parameterize.java
+++ java programes/Parameterize.java
@@ -1,15 +1,16 @@
 
-public class Noargument{
+public class Parameterize{
 int roll_no;
 String name;
-public Noargument(int roll_no, String name){
+public Parameterize(int roll_no, String name){
 this.roll_no=roll_no;
 this.name=name;
 }
 public static void main(String[] args){
-Noargument s=new Noargument(04,"Krushna");
+Parameterize s=new Parameterize(04,"Krushna");
 System.out.println("student information");
 System.out.println("student roll no:"+s.roll_no);
 System.out.println("student name:"+s.name);
 }
 }
+
