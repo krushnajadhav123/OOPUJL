5ba6ddea80229ce3fcd9a9d67e409e202e6529ec
krushnajadhav123 <kj2913280@gmail.com>
2025-08-10 23:47:52 +0530

    Update Defaul.java

--- java programes/Defaul.java
+++ java programes/Defaul.java
@@ -1,15 +1,16 @@
 
 
-public class Parameterized{
+public class Default{
 int roll_no;
 String name;
  
 public static void main(String[] args){
- Parameterized s=new Parameterized();
+ Default s=new Default();
 System.out.println("student information");
 System.out.println("student roll no:"+s.roll_no);
 System.out.println("student name:"+s.name);
 }
 }
+
 
 
